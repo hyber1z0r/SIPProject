0dcade9ac662da862c9eead952cc4ecbf60fc654
hyber1z0r <jakobsky@live.dk>
2015-03-02 12:23:49 +0100

    Created first test for insertTeacher JPA JGA, AK

--- src/test/java/com/ohnana/control/JPATest.java
+++ src/test/java/com/ohnana/control/JPATest.java
@@ -1,7 +1,12 @@
 package com.ohnana.control;
 
+import com.ohnana.interfaces.IJPAManager;
+import com.ohnana.interfaces.ITeacher;
+import com.ohnana.model.Teacher;
+import javax.persistence.Persistence;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
 
 /**
  *
@@ -9,7 +14,25 @@ import static org.junit.Assert.*;
  */
 public class JPATest {
     
-    public JPATest() {
+    private IJPAManager manager;
+    
+    @Before
+    public void setup() {
+        manager = new JPAManager(Persistence.createEntityManagerFactory("com.ohnana_SIPProject"));
+        
     }
+    
+    @Test
+    public void testInsertTeacher() {
+        ITeacher t1 = new Teacher();
+        ITeacher t2 = new Teacher();
+        ITeacher t3 = new Teacher();
+        manager.insertTeacher(t1);
+        manager.insertTeacher(t2);
+        manager.insertTeacher(t3);
+       
+    }
+    
+    
     
 }
