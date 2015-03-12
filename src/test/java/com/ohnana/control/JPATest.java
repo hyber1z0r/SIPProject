9cd5a05d55dc4575637591e33888a52a138e344b
hyber1z0r <jakobsky@live.dk>
2015-03-12 12:20:45 +0100

    Made tests in JPA independent JGA, DF

--- src/test/java/com/ohnana/control/JPATest.java
+++ src/test/java/com/ohnana/control/JPATest.java
@@ -16,49 +16,50 @@ import static org.hamcrest.CoreMatchers.is;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.Before;
-import org.junit.FixMethodOrder;
-import org.junit.runners.MethodSorters;
 
 /**
  *
  * @author jakobgaardandersen
  */
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class JPATest {
 
     private IJPAManager manager;
     private List<ITeacher> teachers;
 
     @Before
-    public void setup() {
+    public void setup() throws Exception {
         manager = new JPAManager(Persistence.createEntityManagerFactory("com.ohnana_SIPProject_TEST"));
         teachers = new ArrayList() {
             {
                 add(new Teacher("Peter Lorensen", "pelo"));
                 add(new Teacher("Anders Kalhauge", "aka"));
                 add(new Teacher("Tobias Grundtvig", "tog"));
+                add(new Teacher("Henrik Hauge", "hau"));
+                add(new Teacher("Caroline Simonsen", "chu"));
+                add(new Teacher("Lars Mortensen", "lam"));
+                add(new Teacher("Palle Bech", "pab"));
+                add(new Teacher("Torben Østrup", "tor"));
+                add(new Teacher("Jette Kreiner-Møller", "jekm"));
             }
         };
+        // insert first 6 teachers, so we can use the other two in another test!
+        for (int i = 0; i < 6; i++) {
+            manager.insertTeacher(teachers.get(i));
+        }
     }
 
     @Test
-    public void test_A_InsertTeacher() throws Exception {
+    public void testInsertTeacher() throws Exception {
         List<ITeacher> oldTs = manager.getAllTeachers();
-        for (ITeacher t : teachers) {
-            manager.insertTeacher(t);
+        for (int i = 6; i < 9; i++) {
+            manager.insertTeacher(teachers.get(i));
         }
         List<ITeacher> ts = manager.getAllTeachers();
         assertEquals(ts.size(), oldTs.size() + 3);
     }
 
-     @Test
-    public void test_B_GetAllProposals() {
-        List<IProposal> allProposals = manager.getAllProposals();
-        assertThat(allProposals.size(), is(0));
-    }
-    
     @Test
-    public void test_C_InsertProposal() throws Exception {
+    public void testInsertProposal() throws Exception {
         // Getting all teachers, and inserting a new proposal
         List<ITeacher> allTeachers = manager.getAllTeachers();
         List<IProposal> oldProposals = manager.getAllProposals();
@@ -69,42 +70,41 @@ public class JPATest {
     }
 
     @Test
-    public void test_D_GetAllProposals() {
-        // dependent on test_b
-        List<IProposal> allProposals = manager.getAllProposals();
-        assertEquals(allProposals.size(), 1);
-    }
-    
-    @Test
-    public void test_E_InsertElectiveSubjects() throws Exception
-    {
+    public void testInsertElectiveSubjects() throws Exception {
+        // insert new proposal, to make test independent of other test cases
+        List<ITeacher> allTeachers = manager.getAllTeachers();
+        List<IProposal> oldProposals = manager.getAllProposals();
+        IProposal p1 = new Proposal("Android", "Learn about the android platform", allTeachers.get(0));
+        manager.insertProposal(p1);
+
         final List<IProposal> allProposals = manager.getAllProposals();
         List<IElectiveSubject> es = new ArrayList() {
-                {
-                    this.add(new ElectiveSubject(allProposals.get(0)));
-                }
+            {
+                this.add(new ElectiveSubject(allProposals.get(0)));
+            }
         };
         manager.insertElectiveSubjects(es);
     }
-    
+
     @Test
-    public void test_F_GetAllElectiveSubjects()
-    {
+    public void testGetAllElectiveSubjects() {
         List<IElectiveSubject> allElectiveSubjects = manager.getAllElectiveSubjects();
         assertThat(allElectiveSubjects.size(), is(1));
     }
-    
+
     @Test
-    public void test_G_InsertStudent() throws Exception{
-        IStudent s1 = new Student ("Damjan Filipovic");
+    public void testInsertStudent() throws Exception {
+        IStudent s1 = new Student("Damjan Filipovic");
         manager.insertStudent(s1);
     }
-    
+
     @Test
-    public void test_H_GetAllStudents(){
+    public void testGetAllStudents() throws Exception {
+        List<IStudent> oldStudents = manager.getAllStudents();
+        IStudent s1 = new Student("Damjan Filipovic");
+        manager.insertStudent(s1);
         List<IStudent> allStudents = manager.getAllStudents();
-         assertThat(allStudents.size(), is(1));
+        assertThat(allStudents.size(), is(oldStudents.size() + 1));
     }
-    
-    
+
 }
